758217b4f5857ce0033f966bdc17148ce7b36a56
rbrennan55 <45131363+rbrennan55@users.noreply.github.com>
2023-05-24 14:18:09 -0400

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "96166b997b196039ad68a786cb08e24e"
+
+# Geoapify API Key
+geoapify_key = "32afefe1e67346ef9353f98569b7f6f8"
+
+# Census Data API
+census_key = "3309ac178ea19f6da41673f3a645d4f276022ce9"
